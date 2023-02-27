6d78db98845cdfa33fc44cb2d4c998bae731b991
Emmanuel Vazquez <emmanuel.vazquez@centralesupelec.fr>
2023-02-27 11:53:40 +0100

    pareto: update

--- gpmpcontrib/optim/pareto.py
+++ gpmpcontrib/optim/pareto.py
@@ -7,7 +7,7 @@
 import time
 import numpy as np
 
-def pareto_dominated(z0, z):
+def pareto_lt(z, z0):
     """
     Get a boolean array corresponding to points in z that Pareto-dominate z0
     """
@@ -15,12 +15,26 @@ def pareto_dominated(z0, z):
     return better
 
 
-def pareto_filter(z, z0):
+def pareto_gt(z, z0):
     """
     Get a boolean array corresponding to points in z that are Pareto-dominated by z0
     """
     worse = (z >= z0).all(axis=-1)
     return worse
+
+
+def pareto_filter(zopt, z):
+    """
+    Returns a boolean array indicating whether each point in z is
+    Pareto-dominated by at least one point in zopt.
+    """
+    n = z.shape[0]
+    nopt = zopt.shape[0]
+    dominated = np.zeros(n, dtype=bool)
+    for j in range(nopt):
+        S = np.all(zopt[j,:] <= z, axis=1)
+        dominated = np.logical_or(dominated, S)
+    return dominated
 
 
 def pareto_points_unsorted(z):
@@ -89,14 +103,20 @@ def plot_pareto(axis, z_opt, color="red"):
             color=color,
         )
 
-
 def dominated_area_2d(z_ref, z_opt):
 
     n = z_opt.shape[0]
+    # sort wrt first dimension
     ind_sort = z_opt[:, 0].argsort()
     z_opt = z_opt[ind_sort]
+
+    # threshold wrt zref
+    z_opt = np.minimum(z_ref, z_opt)
+
+    # set nadir point
     z_0 = np.array([z_opt[0, 0], z_opt[n-1, 1]])
 
+    # box area
     B = (z_ref[0] - z_0[0]) * (z_ref[1] - z_0[1])
 
     if n > 1:
@@ -118,7 +138,42 @@ def symmdiff_area_2d(z_ref, z_opt_1, z_opt_2):
         - dominated_area_2d(z_ref, z_opt_2)
     )
 
+    if np.isnan(symmdiff):
+        __import__("pdb").set_trace()
+
     return symmdiff
+
+def distance(x, y):
+    """Compute a distance matrix
+
+    Parameters
+    ----------
+    x : numpy.array(n,dim)
+        _description_
+    y : numpy.array(m,dim)
+        If y is None, it is assumed y is x, by default None
+    Returns
+    -------
+    numpy.array(n,m)
+        distance matrix such that
+    .. math:: d_{i,j} = (sum_{k=1}^dim (x_{i,k} - y_{i,k})^2)^(1/2)
+    """
+    y2 = np.sum(y**2, axis=1)
+    x2 = np.reshape(np.sum(x**2, axis=1), [-1, 1])
+    d = np.sqrt(x2 + y2 - 2 * np.inner(x, y))
+
+    return d
+
+
+def hausdorff_distance(z1, z2):
+    d = distance(z1, z2)
+    dmin = np.min(d, axis=0)
+    hd = np.max(dmin)
+
+    if np.isnan(hd):
+        __import__("pdb").set_trace()
+
+    return hd
 
 def test_pareto():
     
@@ -151,3 +206,14 @@ def test_pareto():
     z_ref = np.array([1, 1])
     print(dominated_area_2d(z_ref, z1))
     print(symmdiff_area_2d(z_ref, z1, z2))
+
+
+    d = 2;
+    n0 = 10;
+    n1 = int(1e5);
+    z0 = np.random.uniform(0, 1, (n0,d));
+    z1 = np.random.uniform(0, 1, (n1,d));
+    z0_opt_b = pareto.pareto_points(z0)
+    print(pareto.dominated_area_2d(np.array([1, 1]), z0[z0_opt_b]))
+    b = pareto.pareto_filter(z0[z0_opt_b], z1)
+    print(np.sum(b)/n1)
